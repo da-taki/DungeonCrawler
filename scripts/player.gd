79eb72a4a404b0b4b1fe54ad69bcf7866827132f
da-taki <singhtaknoor@gmail.com>
2026-07-24 22:48:10 +0530

    Improved movement and made camera follow player

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,9 @@
 extends CharacterBody2D
 
-var velocity_player: float = 250.0
+var speed: float = 250.0
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("ui_up"):
-		position.y -= velocity_player*delta
-	elif Input.is_action_pressed("ui_down"):
-		position.y += velocity_player*delta
-	elif Input.is_action_pressed("ui_right"):
-		position.x += velocity_player*delta
-	elif Input.is_action_pressed("ui_left"):
-		position.x -= velocity_player*delta
-		
+	var direction:= Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	velocity = direction * speed
 	move_and_slide()
-		
